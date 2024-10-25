53bbd8925b1fa2bdd239f445b52c4b6876a388e8
Ro <u202213765@upc.edu.pe>
2024-10-25 09:57:20 -0500

    feat(enrollments): add enrollment aggregate methods.

--- src/main/java/com/acme/center/platform/learning/domain/model/aggregates/Enrollment.java
+++ src/main/java/com/acme/center/platform/learning/domain/model/aggregates/Enrollment.java
@@ -25,7 +25,52 @@ public class Enrollment extends AuditableAbstractAggregateRoot<Enrollment> {
 
     private EnrollmentStatus status;
 
+    //TODO: Add Progress Record
 
+    //siempre debe haber un constructor sin parametros
+    public Enrollment(){
+    }
 
+    public Enrollment(AcmeStudentRecordId studentRecordId, Course course){
+        this.acmeStudentRecordId = studentRecordId;
+        this.course = course;
+        this.status = EnrollmentStatus.REQUESTED;
+        //TODO: Initialize Progress Record
+    }
+
+    public void confirm(){
+        this.status = EnrollmentStatus.CONFIRM;
+        //TODO: Initialize Progress Record
+
+    }
+
+    public void reject(){
+        this.status = EnrollmentStatus.REJECTED;
+    }
+
+    public void cancel(){
+        this.status = EnrollmentStatus.CANCELLED;
+    }
+
+    public String getStatus(){
+        return status.name().toLowerCase();
+    }
+
+    public boolean isConfirmed(){
+        return status == EnrollmentStatus.CONFIRM;
+    }
+
+    public boolean isRejected(){
+        return status == EnrollmentStatus.REJECTED;
+    }
+
+    public boolean isCancelled(){
+        return status == EnrollmentStatus.CANCELLED;
+    }
+
+    public void completeTutorial(Long tutorialId){
+        //TODO: Update Progress Record
+        // Emit the tutorial Completed Event
+    }
 
 }
