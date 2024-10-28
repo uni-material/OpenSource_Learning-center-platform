a94e55a8165e729c4180e8c189e2379054fe485e
RominaMaita <u202213765@upc.edu.pe>
2024-10-28 16:53:25 -0500

    feat: add repositories for each aggregate.

---
+++ src/main/java/com/acme/center/platform/learning/infrastructure/persistence/jpa/repositories/CourseRepository.java
@@ -0,0 +1,19 @@
+package com.acme.center.platform.learning.infrastructure.persistence.jpa.repositories;
+
+import com.acme.center.platform.learning.domain.model.aggregates.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    Optional<Course> findByTitle(String title);
+
+    boolean existByTitle(String title);
+
+    boolean existsByTitleAndIdIsNot(String title, Long id);
+
+}
